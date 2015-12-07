35150c96c6bc522cd6b934418582b61f4727d881
Mike Cuddy <mcuddy77@gmail.com>
2015-12-07 15:37:32 -0500

    working on the model

--- battleship.js
+++ battleship.js
@@ -16,10 +16,27 @@ var view = {
 	}
 };
 
-view.displayMiss("00");
-view.displayHit("34");
-view.displayMiss("55");
-view.displayHit("12");
-view.displayMiss("25");
-view.displayHit("26");
-view.displayMessage("hello is this working?");
+// view.displayMiss("00");
+// view.displayHit("34");
+// view.displayMiss("55");
+// view.displayHit("12");
+// view.displayMiss("25");
+// view.displayHit("26");
+// view.displayMessage("hello is this working?");
+
+var model = {
+	boardSize: 7,
+	numShips: 3,
+	shipLength: 3, 
+	shipsSunk: 0, 
+	ships: [{ locations: ["06", "16", "26"], hits: ["","",""]},
+					{ locations: ["24", "34", "44"], hits: ["","",""]},
+					{ locations: ["10", "11", "12"], hits: ["","",""]}],
+	fire: function(guess) {
+		for (var i = 0; i < this.numShips; i++) {
+			var ship = this.ship = this.ships[i];
+			var locations = ship.locations
+		}
+	} 
+
+};
