5ce00ef120f16ef5599127b4e40e48f9bd0db4df
Mike Cuddy <mcuddy77@gmail.com>
2015-12-07 15:09:49 -0500

    more work in JS file

--- battleship.js
+++ battleship.js
@@ -14,5 +14,11 @@ var view = {
 		cell.setAttribute("class","miss");
 
 	}
-
 };
+
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
