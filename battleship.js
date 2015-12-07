11480d3f21df2a3c540235342bf5b8209f68ddea
Mike Cuddy <mcuddy77@gmail.com>
2015-12-07 14:43:55 -0500

    working on JS side

--- battleship.js
+++ battleship.js
@@ -1,1 +1,14 @@
+var view = {
+	displayMessage: function(msg) {
+		var messageArea = document.getElementById("messageArea");
+		messageArea.innerHTML = msg;
+	}, 
 
+	displayHit: function(location) {
+
+	},
+	displayMiss: function(location) {
+
+	}
+
+};
